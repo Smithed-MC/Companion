41169d62eb5d467ba27d7360d641ffda8a58c03b
Pepe20129 <72659707+Pepe20129@users.noreply.github.com>
2022-06-12 21:56:00 +0200

    Refactor SmithedDataPackProvider

--- src/main/java/dev/smithed/companion/pack_provider/SmithedDataPackProvider.java
+++ src/main/java/dev/smithed/companion/pack_provider/SmithedDataPackProvider.java
@@ -1,6 +1,8 @@
 package dev.smithed.companion.pack_provider;
 
 import net.minecraft.resource.*;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -12,10 +14,13 @@ import java.util.function.Supplier;
 public class SmithedDataPackProvider extends FileResourcePackProvider {
     private static final FileFilter FILTERED_PACK = (file) -> (file.isFile() && file.getName().endsWith(".zip")) || (file.isDirectory() && (new File(file, "pack.mcmeta")).isFile());
 
+    private static final ResourcePackSource SMITHED_PACK_SOURCE = (text) -> (Text.translatable("pack.nameAndSource", text, Text.literal("smithed"))).formatted(Formatting.AQUA);
+
+    //this is read but never written to
     public List<String> orderedSmithedPacks = new ArrayList<>();
 
     public SmithedDataPackProvider(File packsFolder) {
-        super(packsFolder, SmithedDataPackSource.PACK_SOURCE_SMITHED);
+        super(packsFolder, SMITHED_PACK_SOURCE);
     }
 
     @Override
@@ -25,18 +30,18 @@ public class SmithedDataPackProvider extends FileResourcePackProvider {
             this.packsFolder.mkdirs();
         }
 
-        File[] packs = loadPacksFromFiles();
+        File[] packs = this.packsFolder.listFiles(FILTERED_PACK);
 
         if (packs != null) {
             for (File file : packs) {
                 String packName = file.getName();
                 ResourcePackProfile resourcePackProfile = ResourcePackProfile.of(
-                        packName,
-                        true,
-                        this.createResourcepack(file),
-                        factory,
-                        ResourcePackProfile.InsertionPosition.TOP,
-                        SmithedDataPackSource.PACK_SOURCE_SMITHED
+                    packName,
+                    true,
+                    this.createResourcepack(file),
+                    factory,
+                    ResourcePackProfile.InsertionPosition.TOP,
+                    SMITHED_PACK_SOURCE
                 );
 
                 if (resourcePackProfile != null) {
@@ -44,11 +49,6 @@ public class SmithedDataPackProvider extends FileResourcePackProvider {
                 }
             }
         }
-    }
-
-    private File[] loadPacksFromFiles() {
-        //load files in any order
-        return this.packsFolder.listFiles(FILTERED_PACK);
     }
 
     private Supplier<ResourcePack> createResourcepack(File file) {
